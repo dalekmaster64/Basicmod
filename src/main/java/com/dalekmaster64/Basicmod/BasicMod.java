17db7a6e138e8f4805dab729bd8a1cdb1e1492e3
dalekmaster64 <4242deathsdoor@gmail.com>
2015-09-12 16:31:57 -0400

    Video 2 Com

---
+++ src/main/java/com/dalekmaster64/Basicmod/BasicMod.java
@@ -0,0 +1,29 @@
+package com.dalekmaster64.Basicmod;
+
+import com.dalekmaster64.Basicmod.help.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+
+public class BasicMod 
+{
+		@Mod.EventHandler
+		public void preInit(FMLPreInitializationEvent event)
+		{
+			
+		}
+		@Mod.EventHandler
+		public void Init(FMLInitializationEvent event)
+		{
+			
+		}
+		@Mod.EventHandler
+		public void postInit(FMLPostInitializationEvent event)
+		{
+			
+		}
+}
---
+++ src/main/java/com/dalekmaster64/Basicmod/help/Reference.java
@@ -0,0 +1,9 @@
+package com.dalekmaster64.Basicmod.help;
+
+public class Reference 
+{
+
+	public static final String MODID = "dalekmaster64_basicmod";
+	public static final String NAME = "Basic Mod";
+	public static final String VERSION = "v0.1 alpha";
+}
